6591effa6a2a61eb0fb0aac3b09176b53cbb3980
Captain_Nemo <nemocp82@gmail.com>
2021-12-15 16:16:25 +0300

    work in tiles, support, enemy, game_data

--- tiles.py
+++ tiles.py
@@ -1,15 +1,61 @@
 import pygame
+from support import import_folder
 
 class Tile(pygame.sprite.Sprite):
-    """отвечает за плитку, по которой передвигается персонаж"""
-    def __init__(self, pos, size):
+    """ класс блока """
+    def __init__(self, size, x, y):
         super().__init__()
-        self.image = pygame.Surface((size, size))#квадратные блоки
-        self.image.fill('grey')
-        self.rect = self.image.get_rect(topleft = pos)
+        self.image = pygame.Surface((size, size))
+        self.rect = self.image.get_rect(topleft = (x, y))
 
-    def update(self, x_shift):
-        """графически выводит"""
-        self.rect.x += x_shift
+    def update(self, shift):
+        self.rect.x += shift
         
+class StaticTile(Tile):
+    """ класс неподвижного блока """
+    def __init__(self, size, x, y, surface):
+        super().__init__(size, x, y)
+        self.image = surface
     
+class Crate(StaticTile):
+    """ класс ящика """
+    def __init__(self, size, x, y):
+        super().__init__(size, x, y, pygame.image.load('./graphics/terrain/crate.png').convert_alpha())
+        offset_y = y + size
+        self.rect = self.image.get_rect(bottomleft = (x, offset_y))
+        
+class AnimatedTile(Tile):
+    """ класс движущихся элементов уровня """
+    def __init__(self, size, x, y, path):
+        super().__init__(size, x, y)
+        self.frames = import_folder(path)
+        self.frame_index = 0
+        self.image = self.frames[self.frame_index]
+        
+    def animate(self):
+        self.frame_index += 0.15
+        if self.frame_index >= len(self.frames):
+            self.frame_index = 0
+        self.image = self.frames[int(self.frame_index)]
+
+    def update(self, shift):
+        self.rect.x += shift
+        self.animate()
+
+class Coin(AnimatedTile):
+    """ класс монеток """
+    def __init__(self, size, x, y, path):
+        super().__init__(size, x, y, path)
+        center_x = x + int(size / 2)
+        center_y = y + int(size / 2)
+        self.rect = self.image.get_rect(center = (center_x, center_y))
+
+class Palm(AnimatedTile):
+    """ класс пальм """
+    def __init__(self, size, x, y, path, offset):
+        super().__init__(size, x, y, path)
+        offset_y = y - offset
+        self.rect.topleft = (x, offset_y)
+
+
+            
