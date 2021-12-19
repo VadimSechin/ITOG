acb240ba4a2009b31ae5f9f479f46214da13f0c3
Vlad00Kharav1n1n <kharavinin.va@phystech.edu>
2021-12-20 01:32:34 +0300

    Рефакторинг

--- level.py
+++ level.py
@@ -10,6 +10,66 @@ from game_data import levels
 
 
 class Level:
+    """ Класс Level используется для создания игрового уровня
+
+    Основное применение - создание игровых спрайтов, их перемещение, коллизии
+
+    Atributes
+
+    ---------
+    display_surface : surface
+        поверхность игрового уровня
+    world_shift : int
+        скорость перемещения камеры
+    current_x : int
+        положение игрового персонажа по оси х
+    stomp_sound : mp3_file
+        музыка нанесения урона
+    coin_sound : mp3_file
+        музыка собрания монеток
+    create_overworld : function
+        функция создающая карту уровней
+    current_level : int
+        номер текущего уровня, начиная с 0
+    new_max_level : library
+        номер максимального доступного уровня, начиная с 0
+    player : GroupSingle
+        спрайт игрового персонажа
+    player_setup : function
+        создание спрайта игрового персонажа и точки завершения уровня
+    change_coins : function
+        функция обновляющая число собранных монет
+    dust_sprite : GroupSingle
+        спрайт эффектов движения персонажа
+    player_on_ground : bull
+        утверждение, является ли персонаж на поверхности игровых спрайтов
+    explosion_sprites : Group
+        спрайты эффектов убийства врага
+    terrain_sprites : Group
+        спрайты ландшафта
+    grass_sprites : Group
+        спрайты травы
+    crate_sprites : Group
+        спрайты ящиков
+    coin_sprites : Group
+        спрайты монеток
+    fg_palm_sprites : Group
+        спрайты пальм переднего плана
+    bg_palm_sprites : Group
+        спрайты пальм заднего плана
+    enemy_sprites : Group
+        спрайты врагов
+    constraint_sprites : Group
+        спрайты точек поворота врагов
+    sky : class Sky instance
+        объект класа Sky, отображает небо
+    water : class Water instance
+        объект класа  Water, отображает воду
+    clouds : class Clouds instance
+        объект класа Clouds, отображает облака
+
+    """
+    
     def __init__(self, current_level, surface, create_overworld, change_coins, change_health):
         # general setup
         self.display_surface = surface
@@ -82,9 +142,13 @@ class Level:
         
 
     def create_tile_group(self, layout, type):
-        """ Из обработанного csv файла получаем координаты спрайтов,
-            согласно координатам сохраняем картинки,
-            вся информация о спрайтах хранится в группе"""
+        """ Метод, который из обработанного csv файла получает координаты спрайтов,
+            согласно координатам создает игровые спрайты,
+            вся информация о спрайтах хранится в группе
+
+            return :
+            Группа игровых спрайтов"""
+        
         sprite_group = pygame.sprite.Group()
 
         for row_index, row in enumerate(layout):
@@ -137,12 +201,15 @@ class Level:
         return sprite_group
 
     def enemy_constraint_collision(self):
-        """ при столкновении NPC с преградой он поворачивает """
+        """ Метод, который изменяет направление движения врагов при столкновении врагов с точками поворота """
+
         for enemy in self.enemy_sprites.sprites():
             if pygame.sprite.spritecollide(enemy, self.constraint_sprites, False):
                 enemy.reverse()
                 
     def create_jump_particles(self, pos):
+        """ Метод, который создает спрайт эффект прыжка персонажа """
+
         if self.player.sprite.facing_right:
             pos -= pygame.math.Vector2(10, 18)
         else:
@@ -151,6 +218,8 @@ class Level:
         self.dust_sprite.add(jump_particle_sprite)
 
     def horisontal_movement_collision(self):
+        """ Метод, описывающий все горизонтальное движение игрового персонажа"""
+
         player = self.player.sprite
         player.collision_rect.x += player.direction.x * player.speed
         collidable_sprites = self.terrain_sprites.sprites() + self. crate_sprites.sprites() + self.fg_palm_sprites.sprites()
@@ -171,13 +240,16 @@ class Level:
             player.on_right = False
 
     def get_player_on_ground(self):
+        """ Метод, проверяющий является ли игровой персонаж на поверхности игровых спрайтов """
+
         if self.player.sprite.on_ground:
             self.player_on_ground = True
         else:
             self.player_on_ground = False
 
     def create_landing_dust(self):
-        # вручную устанавливается уровень картинок
+        """ Метод, создающий спрайт эффекта приземления """
+        
         if not self.player_on_ground and self.player.sprite.on_ground and not self.dust_sprite.sprites():
             if self.player.sprite.facing_right:
                 offset = pygame.math.Vector2(10, 20)
@@ -187,6 +259,8 @@ class Level:
             self.dust_sprite.add(fall_dust_particle)
                 
     def vertical_movement_collision(self):
+        """ Метод, описывающий все вертикальное движение персонажа """
+        
         player = self.player.sprite
         player.apply_gravity()
         collidable_sprites = self.terrain_sprites.sprites() + self. crate_sprites.sprites() + self.fg_palm_sprites.sprites()
@@ -205,6 +279,17 @@ class Level:
             player.on_ground = False
         
     def player_setup(self, layout, change_health):
+        """ Метод, создающий спрайт игрового персонажа и точку завершения уровня
+
+        Parametrs
+        ---------
+
+        layout : list
+            лист листов значений id объектов 
+        change_health : function
+            проверяет количество собранных монет
+        """
+        
         for row_index, row in enumerate(layout):
             for column_index, value in enumerate(row):
                 x = column_index * tile_size
@@ -218,7 +303,9 @@ class Level:
                     self.goal.add(sprite)
                     
     def scroll_x(self):
-        """ Движение экрана  """
+        """ Метод, изменяющий скорость движение камеры,
+            в засисимости от положения и скорости игрового персонажа """
+        
         player = self.player.sprite
         player_x = player.rect.centerx
         direction_x  = player.direction.x
@@ -236,22 +323,30 @@ class Level:
             player.speed = 7
 
     def check_death(self):
+        """ Метод, обновляющий уровень при падении в воду """
+
         if self.player.sprite.rect.top >= screen_height:
             self.create_overworld(self.current_level, 0)
 
     def check_win(self):
+        """ Метод, создающий карту уровней и открывающий следующий
+            уровнень при попадании персонажа в точку заверщения уровня """
+        
         if pygame.sprite.spritecollide(self.player.sprite, self.goal, False):
             self.create_overworld(self.current_level, self.new_max_level)
 
     def check_coin_collisions(self):
+        """ Метод, который считает количество собранных монет """
+        
         collided_coins = pygame.sprite.spritecollide(self.player.sprite, self.coin_sprites, True)
         if collided_coins:
             self.coin_sound.play()
             for coin in collided_coins:
                 self.change_coins(coin.value)
 
-    def enemy_behavoir(self):
-
+    def enemy_ai_behaviour(self):
+        """ Метод, который заставляет врагов следовать за персонажем если он находится неподалеку """
+        
         for enemy in self.enemy_sprites:
             enemy_x = enemy.rect.centerx
             enemy_top = enemy.rect.centery - 32
@@ -264,6 +359,8 @@ class Level:
                 
 
     def check_enemy_collisions(self):
+        """ Метод, обрабатывающий все столкновения персонажа и врагов """
+        
         enemy_collisions = pygame.sprite.spritecollide(self.player.sprite, self.enemy_sprites, False)
 
         if enemy_collisions:
@@ -280,7 +377,7 @@ class Level:
                 else:
                     self.player.sprite.get_damage()
     def run(self):
-        """ запускает всю игру / уровень """
+        """ Метод, который запускает уровень """
 
         # небо
         self.sky.draw(self.display_surface)
@@ -305,7 +402,7 @@ class Level:
         self.enemy_constraint_collision() # поворачивает противников на границе
         self.explosion_sprites.update(self.world_shift)
         self.explosion_sprites.draw(self.display_surface)
-        self.enemy_behavoir()
+        self.enemy_ai_behaviour()
 
         # трава
         self.grass_sprites.draw(self.display_surface)
