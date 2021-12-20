e16966258432946e27f306f74e63900abd606137
Captain_Nemo <nemocp82@gmail.com>
2021-12-20 15:15:10 +0300

    рефакторинг maim.py

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from level import Level
 from shop import Shop
 from ui import UI
 
+
 class Game:
+    """
+    game class
+    """
+
     def __init__(self):
         # game attributes
         self.max_level = 0
@@ -18,38 +23,60 @@ class Game:
         # overworld creation
         self.overworld = Overworld(0, self.max_level, screen, self.create_level, self.create_shop)
         self.status = 'overworld'
-        self.overworld_bg_music.play(loops = -1)
+        self.overworld_bg_music.play(loops=-1)
         # user interface
         self.ui = UI(screen)
-        
 
     def create_level(self, current_level):
+        """
+        creates level
+        :param current_level: current level
+        """
         self.level = Level(current_level, screen, self.create_overworld, self.change_coins, self.change_health)
         self.status = 'level'
         self.overworld_bg_music.stop()
-        self.level_bg_music.play(loops = -1)
+        self.level_bg_music.play(loops=-1)
 
     def create_shop(self):
+        """
+        creates shop
+        """
         self.shop = Shop(screen, self.create_overworld)
         self.status = 'shop'
         self.overworld_bg_music.stop()
-        self.level_bg_music.play(loops = -1)
+        self.level_bg_music.play(loops=-1)
 
     def create_overworld(self, current_level, new_max_level):
+        """
+        creates overworld
+        :param current_level: current level
+        :param new_max_level: new_max_level
+        """
         if new_max_level > self.max_level:
             self.max_level = new_max_level
         self.overworld = Overworld(current_level, self.max_level, screen, self.create_level, self.create_shop)
         self.status = 'overworld'
         self.level_bg_music.stop()
-        self.overworld_bg_music.play(loops = -1)
+        self.overworld_bg_music.play(loops=-1)
 
     def change_coins(self, amount):
+        """
+        changes coins amount
+        :param amount: amount of coins to add to the balance
+        """
         self.coins += amount
 
     def change_health(self, amount):
+        """
+        changes heath amount
+        :param amount: amount of health to add to the balance
+        """
         self.current_health += amount
 
     def buy_potion(self, price):
+        """
+        :param price: price of tje health potion
+        """
         if self.coins >= price:
             self.coins -= price
             if self.current_health <= 95:
@@ -60,9 +87,10 @@ class Game:
         else:
             print('hasnt bought')
 
-
-
     def check_game_over(self):
+        """
+        checks if the game is over
+        """
         if self.current_health <= 0:
             self.current_health = 100
             self.coins = 0
@@ -70,9 +98,12 @@ class Game:
             self.overworld = Overworld(0, self.max_level, screen, self.create_level, self.create_shop)
             self.status = 'overworld'
             self.level_bg_music.stop()
-            self.overworld_bg_music.play(loops = -1)
-        
+            self.overworld_bg_music.play(loops=-1)
+
     def run(self):
+        """
+        runs game
+        """
         if self.status == 'overworld':
             self.overworld.run()
         if self.status == 'shop':
@@ -85,13 +116,14 @@ class Game:
             self.ui.show_coins(self.coins)
             self.check_game_over()
 
+
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 game = Game()
 
-
-flag = 1
+# running cicle
+flag = 1  # flag to prevent issues with exit from th eshop
 while True:
     for event in pygame.event.get():
         flag = 1
@@ -99,20 +131,18 @@ while True:
             pygame.quit()
             sys.exit()
         if (game.status == 'overworld') and (event.type == pygame.MOUSEBUTTONDOWN) and (flag == 1):
-            if (70>event.pos[0]>30) and (70>event.pos[1]>30):
+            if (70 > event.pos[0] > 30) and (70 > event.pos[1] > 30):
                 game.create_shop()
                 flag = -flag
         if (game.status == 'shop') and (event.type == pygame.MOUSEBUTTONDOWN) and (flag == 1):
-            if (1140>event.pos[0]>1100) and (70>event.pos[1]>30):
+            if (1140 > event.pos[0] > 1100) and (70 > event.pos[1] > 30):
                 game.create_overworld(0, game.max_level)
                 flag = -flag
 
         if (game.status == 'shop') and (event.type == pygame.MOUSEBUTTONDOWN) and (flag == 1):
-            if (270>event.pos[0]>230) and (270>event.pos[1]>230):
+            if (270 > event.pos[0] > 230) and (270 > event.pos[1] > 230):
                 game.buy_potion(10)
                 flag = -flag
-
-
 
     game.run()
     pygame.display.update()
